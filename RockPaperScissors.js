1686251f7bed1a23c4ce5e451e9f85cfefb7625e
kristentyrrell <krissietyrrell@gmail.com>
2016-12-11 17:31:24 -0500

    Math Fix
    More precise math on the odds fraction

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,8 +1,8 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-if (computerChoice < 0.34) {
+if (computerChoice < (1/3)) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} else if(computerChoice >= (2/3)) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
